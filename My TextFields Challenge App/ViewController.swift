212d2debe13b75b52ef0ff3f3908f8e20973d1ba
WaterfallDeveloper <joe.z@wolke7.net>
2015-11-18 15:01:31 +0100

    add outlets

--- My TextFields Challenge App/ViewController.swift
+++ My TextFields Challenge App/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var cashTextField: UITextField!
+    @IBOutlet weak var lockedTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
